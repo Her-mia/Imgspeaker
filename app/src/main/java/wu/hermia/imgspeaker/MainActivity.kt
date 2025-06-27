bc48ebda71cc81d52bc1ad812b8768b8e85d339b
hermia <hermia.wu@outlook.com>
2025-06-27 20:46:02 +0800

    add image

--- app/src/main/java/wu/hermia/imgspeaker/MainActivity.kt
+++ app/src/main/java/wu/hermia/imgspeaker/MainActivity.kt
@@ -1,17 +1,19 @@
 package wu.hermia.imgspeaker
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import wu.hermia.imgspeaker.ui.theme.ImgspeakerTheme
+import androidx.compose.ui.res.painterResource
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +22,28 @@ class MainActivity : ComponentActivity() {
         setContent {
             ImgspeakerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    ImageView(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun ImageView(modifier: Modifier=Modifier) {
+    val imageModifier = Modifier
+        .size(150.dp)
+    val image = painterResource(R.drawable.typewriter)
+    Image(
+        painter = image,
+        contentDescription = null,
+        modifier = Modifier)}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ImgspeakerTheme {
-        Greeting("Android")
+        ImageView()
     }
 }
